eea7e0a35f51c8cf3a5f2fd957f2a03a7025b1ce
Aravinth <79074310+Aravinth1525@users.noreply.github.com>
2021-06-09 19:37:56 +0530

    Delete Python directory

--- Python/QR Code.py
+++
@@ -1,4 +0,0 @@
-import pyqrcode
-Msg= 'https://1.bp.blogspot.com/-IO4xROpdoLw/YCF79K5MVKI/AAAAAAAADO8/cPlcKBjm4vwbgz3YcYGqfbS2xTIcfpwoACLcBGAsYHQ/s2048/IMG_2342.jpg'
-x=pyqrcode.create(Msg)
-x.png('Blog.png',scale=20)
--- Python/String.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter The String : ")
-b=a.find('$')
-if (b!=-1):
-    d=[b.replace('$',"@$") for b in a]
-    e="".join(d)
-    print(d)
-    print('$'+e+'$')
-else:
-    print('$'+a+'$')
